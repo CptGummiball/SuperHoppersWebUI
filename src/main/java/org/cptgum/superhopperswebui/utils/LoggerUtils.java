3cb13f359f60d091932df10be838b08e39f32625
CptGummiball <treeman1992@outlook.de>
2024-02-14 10:04:31 +0100

    Fixed Logger Utils
    Logger Utils dont need Colors!
    added Logfile generator

--- src/main/java/org/cptgum/superhopperswebui/utils/LoggerUtils.java
+++ src/main/java/org/cptgum/superhopperswebui/utils/LoggerUtils.java
@@ -1,6 +1,6 @@
 package org.cptgum.superhopperswebui.utils;
 
-import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,27 +12,50 @@ import java.util.Date;
 
 public class LoggerUtils {
 
+    private static JavaPlugin plugin;
+    public static void setPlugin(JavaPlugin plugin) {
+        LoggerUtils.plugin = plugin;
+    }
+    private static final String DEBUG_LOG_PATH = "plugins/SuperHoppersWebUI/logs/debug.log";
+    private static final String ERROR_LOG_PATH = "plugins/SuperHoppersWebUI/logs/error.log";
     private static final Logger logger = LoggerFactory.getLogger("SuperHoppersWebUI");
 
     public static void logInfo(String message) {
-        logger.info(ChatColor.BLUE + "[SuperHoppersWebUI] " + ChatColor.RESET + message);
+        logger.info(message);
     }
 
     public static void logWarning(String message) {
-        logger.warn(ChatColor.BLUE + "[SuperHoppersWebUI] " + ChatColor.YELLOW + message);
+        logger.warn(message);
     }
 
     public static void logError(String message) {
-        logger.error(ChatColor.BLUE + "[SuperHoppersWebUI] " + ChatColor.RED + message);
+        logger.error(message);
         writeErrorToFile(message);
     }
 
+    public static void logDebug(String message) {
+        writeDebugToFile(message);
+        if (plugin != null && plugin.getConfig().getBoolean("DebugMode", false)) {
+            logger.info(message);
+        }
+    }
+
     private static void writeErrorToFile(String message) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter("error.log", true))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(ERROR_LOG_PATH, true))) {
             String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
             writer.println(timestamp + " - " + message);
         } catch (IOException e) {
             e.printStackTrace(); // Log the error, not be replaceable by logger output because it is an internal error of the logger
         }
     }
+
+    private static void writeDebugToFile(String message) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(DEBUG_LOG_PATH, true))) {
+            String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            writer.println(timestamp + " - " + message);
+        } catch (IOException e) {
+            e.printStackTrace(); // Log the error, not be replaceable by logger output because it is an internal error of the logger
+        }
+    }
+
 }
